35c3cbb8273d10c133f14ca3202fdebb78622cc3
Rusty0201 <ashamano263@gmail.com>
2022-11-21 15:13:04 +0530

    send a push

---
+++ src/org/test/A.java
@@ -0,0 +1,13 @@
+package org.test;
+
+public class A {
+	public void sample() {
+System.out.println("Selenium");
+		
+	}
+	public static void main(String[] args) {
+		A a=new A();
+		a.sample();
+	}
+
+}
